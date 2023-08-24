9436d1df74c80a7ef32687a40491ef2dd4f5f0f5
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-25 00:47:51 +0700

    4 files modified, 1 added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-   	<div id="app">
+   	
 			<div class="container container22243">
 				<div class="diygw-title diygw-top solid-bottom" style="color: white; background: #ff32ad">
 					<div class="title flex1 text-center">หน้าหลัก</div>
 				</div>
-  </div>
+ 
 
   <div>
   <slot/>
