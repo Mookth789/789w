6c3960031ad90395faf394225a324061952dfdb5
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-24 23:55:20 +0700

    1 file modified, 2 added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,53 @@
 <template>
   <div>
-   <h1>Layout</h1>
+   	<div id="app">
+			<div class="container container22243">
+				<div class="diygw-title diygw-top solid-bottom" style="color: white; background: #ff32ad">
+					<div class="title flex1 text-center">หน้าหลัก</div>
+				</div>
   </div>
+
   <div>
   <slot/>
   </div>
+
+
+
+	<div class="flex flex-wrap diygw-col-24 justify-around diygw-bottom flex44-clz">
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+						<span class="flex icon3 diygw-col-0 icon3-clz diy-icon-home"></span>
+						<div class="diygw-col-0">หน้าหลัก</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+						<span class="flex icon4 diygw-col-0 icon4-clz diy-icon-discover"></span>
+						<div class="diygw-col-0">ภารกิจ</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column justify-between items-center flex47-clz">
+						<span class="flex icon5 diygw-col-0 green icon5-clz tiaodong diy-icon-cameraadd"></span>
+						<div class="diygw-col-0">อันดับ</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+						<span class="flex icon6 diygw-col-0 icon6-clz diy-icon-message"></span>
+						<div class="diygw-col-0 text59-clz">10</div>
+						<div class="diygw-col-0">แชท</div>
+					</div>
+					<div class="flex flex-wrap diygw-col-0 flex-direction-column items-center">
+						<span class="flex icon7 diygw-col-0 icon7-clz diy-icon-people"></span>
+						<div class="diygw-col-0">ข้อมูล</div>
+					</div>
+				</div>
+
+
+
+
+	<div class="version text-grey flex-direction-column flex diygw-col-24 justify-center align-center">
+		<div>แพลตฟอร์มสร้างรายได้ออนไลน์</div>
+					<div>www.example.com</div>
+	</div>
+				<div class="clearfix"></div>
+
+</div>
+</div>
 </template>
 
 
@@ -14,5 +57,7 @@
 </script>
 
 <style>
+@import "/assets/css/index.css";
+
 
 </style>
